678a2ec88357abc1fe90d26e95ff2700c4f3a43c
Samuel VIVIEN <samuel.aime.vivien@free.fr>
2021-04-24 23:48:32 +0200

    started compilation proff

--- Projet_SAV_P3.v
+++ Projet_SAV_P3.v
@@ -1,30 +1,119 @@
 Require Import Lia.
+Require Import ssreflect.
 
-Inductive lambdaTerme :=
-  | var : nat -> lambdaTerme
-  | lambda : lambdaTerme -> lambdaTerme
-  | app : lambdaTerme -> lambdaTerme -> lambdaTerme.
+(* Partie 1 : Indices de De Bruijn et manipulation de lambdaTerme *)
 
-Fixpoint linkDepth (t : lambdaTerme) : nat :=
-  match t : lambdaTerme return nat with
+(* - 1 - *)
+
+(* type pour les lambda-termes avec indices de De Bruijn *)
+Inductive lambdaTermeN :=
+  | var : nat -> lambdaTermeN
+  | lambda : lambdaTermeN -> lambdaTermeN
+  | app : lambdaTermeN -> lambdaTermeN -> lambdaTermeN.
+
+(* - 2 - *)
+
+(* une fonction test calulant la profondeur d'un terme *)
+Fixpoint linkDepth (t : lambdaTermeN) : nat :=
+  match t : lambdaTermeN return nat with
     | var _ => 0
     | lambda t_0 => 1 + (linkDepth t_0)
     | app t_0 t_1 => max (linkDepth t_0) (linkDepth t_1)
- end.
+  end.
 
-Definition id : lambdaTerme := lambda (var 0).
+Definition id : lambdaTermeN := lambda (var 0).
 
-Eval compute in (linkDepth id).
+(* [ prop_aux_with_linkDepth n d t ] renvoie la proposition correspondant à
+  "dans le sous-terme t, apparaissant sous d lambdas dans un terme, toutes les variables libres sont d'indice <= n" *)
+Fixpoint prop_aux_with_linkDepth (n : nat) (d : nat) (t : lambdaTermeN) : Prop :=
+  match t return Prop with
+    | var x => (x>n -> x<d)
+    | lambda t_0 => (prop_aux_with_linkDepth n (d+1) t_0)
+    | app t_0 t_1 => (prop_aux_with_linkDepth n d t_0) /\ (prop_aux_with_linkDepth n d t_1)
+  end.
+
+(* hasAllFreeVarUnder n t renvoie la proposition correspondant à 
+  "dans le terme t, toutes les variables libres sont d'indice <= n" *)
+Definition hasAllFreeVarUnder : nat -> lambdaTermeN -> Prop :=
+  fun n => prop_aux_with_linkDepth n 0.
+
+(* la notation proposée par le sujet : *)
+Notation "C[ n ]( t  )" := (hasAllFreeVarUnder (n : nat) (t : lambdaTermeN)).
+
+Lemma lemma0 : forall t : lambdaTermeN, forall n d : nat,
+  prop_aux_with_linkDepth n d t -> prop_aux_with_linkDepth (n+1) d t.
+Proof.
+  move => t.
+  induction t.
+  simpl; lia.
+  simpl; move => n d.
+  apply IHt.
+  simpl; move => n d.
+  case.
+  move => Ht1 Ht2.
+  split; [apply IHt1 | apply IHt2]; trivial.
+Qed.
+
+Lemma lemma1 : forall t : lambdaTermeN, forall n : nat, C[n](t) -> C[n+1](t).
+Proof.
+  move => t n; apply lemma0.
+Qed.
+
+(* isClosed t donne la proposition correspondant à "t est un terme clos" *)
+Definition isClosed t := C[0](t).
+
+(*
+Ici, on définit une convention sur l'interprétation des termes.
+A priori, pour le terme app (var 1) (lambda (var 1)) ou app (var 1) (lambda (var 2)) rien ne permet d'identifier les variables 1 et/ou 2.
+Cependant, lorsque l'on éffectue une beta-réduction sur app (lambda (app (var 1) (lambda (var 1)))) (var 2),
+on n'identifie pas les deux occurrences de 1 à une même variable (celle à substituer par 2)
+et dans app (lambda (lambda (app (var 1) (lambda (var 2))))) (var 2) lors d'une beta-reduction, on veut identifier les occurences de 1 et de 2 à la même variable que l'on substitue.
+Donc on choisit la convention que chaque lambda incrémente de 1 les valeurs représentant les variables libres auquelles elles font référence.
+Par exemple lambda (app (lambda (var 2)) (var 1)) s'interprète comme le lambda-Terme lambdaX.((lambdaY. Z) Z) 
+*)
+
+(* remarque : dans la suite on utilise Nat.eqb, Nat.leb pour obtenir un résultat booléen et non propositionnel pour (3 = 4) *)
+
+(* incr_free_with_linkDepth d u ncrémente toutes les variables libres de u en tant que sous terme d'un
+  autre terme, apparaissant sous une profondeur de d lambdas. *)
+
+Fixpoint incr_free_with_linkDepth (d : nat) (u : lambdaTermeN) :=
+ match u return lambdaTermeN with
+    | var x => if (Nat.leb d x) then (var (S x)) else u
+    | lambda u_0 => lambda (incr_free_with_linkDepth (S d) u_0)
+    | app u_0 u_1 => app (incr_free_with_linkDepth d u_0) (incr_free_with_linkDepth d u_1)
+  end.
+
+Definition incr_free t := incr_free_with_linkDepth 0 t. 
+
+(* On définit la fonction de substitution :
+subst t y u renvoie le terme t dans lequel on a substitué la variable y (au sens discuté précédemment par le terme u *)
+Fixpoint subst (t : lambdaTermeN) (y : nat) (u : lambdaTermeN) : lambdaTermeN := 
+  match t return lambdaTermeN with
+    | var x =>
+      if (Nat.eqb x y) then u else t
+    | lambda t_0 =>
+      lambda (subst t_0 (y+1) (incr_free u))
+    | app t_0 t_1 =>
+      app (subst t_0 y u) (subst t_1 y u)
+  end.
+
+Notation "t [ y <- u ]" := (subst t y u) (at level 0).
+
+(*
+Eval compute in id.
+Eval compute in ( ((app (lambda (var 1)) (var 1))) [ 0 <- id ] ).
+*)
+
+
+
 
 
 (* Définition du code de la machine de Krivine *)
-CoInductive instruction :=
-    | access : nat -> instruction
-    | grab : instruction
-    | push : codeBloc -> instruction
-with codeBloc := 
-    | emptyBloc : codeBloc
-    | ins : instruction -> codeBloc -> codeBloc.
+Inductive codeBloc :=
+    | access : nat -> codeBloc
+    | grab : codeBloc -> codeBloc
+    | push : codeBloc -> codeBloc -> codeBloc.
 
 (* Définition de la pile de la machine de Krivine *)
 Inductive stack := 
@@ -37,30 +126,75 @@ Definition krivineState: Type := (codeBloc * stack * stack).
 (* Définition de la fonction transition *)
 Definition transitionFunction : krivineState -> option krivineState :=
   fun ks => match ks return option krivineState with 
-    | (ins (access 0) _, element (c_0, e_0) _, s) => Some (c_0, e_0, s)
-    | (ins (access (S n)) c, element _ tl, s) =>  Some (ins (access n) c, tl, s)
-    | (ins (push c_2) c, e, s) => Some (c, e, element (c_2, e) s)
-    | (ins grab c, e, element (c_0, e_0) s) => Some (c, element (c_0, e_0) e, s)
+    | (access 0, element (c_0, e_0) _, s) => Some (c_0, e_0, s)
+    | (access (S n), element _ tl, s) =>  Some (access n, tl, s)
+    | (push c_2 c, e, s) => Some (c, e, element (c_2, e) s)
+    | (grab c, e, element (c_0, e_0) s) => Some (c, element (c_0, e_0) e, s)
     | _ => None
 end.
 
-Fixpoint comp (t: lambdaTerme) : codeBloc :=
-  match t: lambdaTerme return codeBloc with
-    | var n => ins (access n) emptyBloc
-    | lambda t_0 => ins grab (comp t_0)
-    | app t_0 t_1 => ins (push (comp t_1)) (comp t_0)
+Fixpoint comp (t: lambdaTermeN) : codeBloc :=
+  match t: lambdaTermeN return codeBloc with
+    | var n => access n
+    | lambda t_0 => grab (comp t_0)
+    | app t_0 t_1 => push (comp t_1) (comp t_0)
 end.
+
+Definition comp_glob : lambdaTermeN -> krivineState := 
+  fun t => (comp t, empty, empty).
 
 Print comp.
 
-Fixpoint tau_code (code : codeBloc) : lambdaTerme :=
-  match code return lambdaTerme with
-    | ins (access n) _ => var n
-    | ins (push c_2) c_1 => app (tau_code c_1) (tau_code c_2)
-    | ins grab c => lambda (tau_code c)
+Fixpoint tau_code (code: codeBloc) : lambdaTermeN :=
+  match code return lambdaTermeN with
+    | access n => var n
+    | push c_2 c_1 => app (tau_code c_1) (tau_code c_2)
+    | grab c => lambda (tau_code c)
 end.
 
-Fixpoint 
+Fixpoint tau_tuple (c: codeBloc) (e: stack) (prof: nat) : lambdaTermeN :=
+  match e with
+    | empty => tau_code c
+    | element (c_0, e_0) e_1 => subst (tau_tuple c e_1 (S prof)) prof (tau_tuple c_0 e_0 0)
+end.
 
+Fixpoint tau_inner (c: codeBloc) (e: stack) (s: stack) : lambdaTermeN :=
+  match s return lambdaTermeN with
+    | element (c_0, e_0) tl => app (tau_tuple c e 0) (tau_inner c_0 e_0 tl)
+    | empty => tau_tuple c e 0
+end.
 
 Print tau_code.
+Print tau_tuple.
+Print tau_inner.
+
+
+
+Definition tau : krivineState -> lambdaTermeN := 
+  fun ks =>  
+    match ks return lambdaTermeN with 
+      | (c, e, s) => tau_inner c e s
+end.
+
+Lemma comp_is_comp_glob: forall t: lambdaTermeN, tau (comp_glob t) = tau_code (comp t).
+Proof.
+  move => t.
+  simpl.
+  trivial.
+Qed.
+
+Lemma comp_and_tau: forall t: lambdaTermeN, tau (comp_glob t) = t.
+Proof.
+  induction t; simpl.
+  trivial.
+  suff : (tau_code (comp t)) = t.
+  move => H; rewrite H; trivial.
+  rewrite <- comp_is_comp_glob.
+  trivial.
+  rewrite <- comp_is_comp_glob.
+  rewrite IHt1.
+  rewrite <- comp_is_comp_glob.
+  rewrite IHt2.
+  trivial.
+Qed.
+
