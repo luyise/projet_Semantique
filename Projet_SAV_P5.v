3b62d73661d31a490637f7e3d071c9230731654e
Samuel VIVIEN <samuel.aime.vivien@free.fr>
2021-04-29 17:28:47 +0200

    modified structure

--- Projet_SAV_P5.v
+++ Projet_SAV_P5.v
@@ -43,61 +43,6 @@ Definition state_correctness: krivineState -> Prop :=
     | (c, e, s) => code_correctness c e /\ stack_correctness e /\ stack_correctness s
 end.
 
-Lemma option_equality_lemma: forall A : Type, forall (a1 a2: A), Some a1 = Some a2 <-> a1 = a2.
-Proof.
-  move => A a1 a2.
-  split.
-  case.
-  trivial.
-  move => H.
-  rewrite H.
-  trivial.
-Qed.
-
-Lemma tuple_eq_is_eq: forall (A B : Type) (a1 a2 : A) (b1 b2 : B),
-      (a1, b1) = (a2, b2) <-> a1 = a2 /\ b1 = b2.
-Proof.
-  intros.
-  split; intro H . split.
-  1 : replace a1 with (fst (a1, b1)); replace a2 with (fst (a2, b2)); trivial.
-  2 : replace b1 with (snd (a1, b1)); replace b2 with (snd (a2, b2)); trivial.
-  1-2: rewrite H.
-  1-2: trivial.
-  destruct H.
-  rewrite H H0.
-  trivial.
-Qed.
-
-Lemma triple_eq_is_eq: forall (A B C: Type) (a1 a2 : A) (b1 b2 : B) (c1 c2 : C),
-  (a1, b1, c1) = (a2, b2, c2) <-> a1 = a2 /\ b1 = b2 /\ c1 = c2.
-  intros.
-  split.
-  intro H.
-  repeat split.
-  1 : replace a1 with (fst (fst ((a1, b1), c1))); replace a2 with (fst (fst ((a2, b2), c2))); trivial.
-  2 : replace b1 with (snd (fst ((a1, b1), c1))); replace b2 with (snd (fst ((a2, b2), c2))); trivial.
-  3 : replace c1 with (snd ((a1, b1), c1)); replace c2 with (snd ((a2, b2), c2)); trivial.
-  1-3: rewrite H; trivial.
-  intro H. destruct H; destruct H0.
-  rewrite H H0 H1.
-  trivial.
-Qed.
-
-Lemma some_triple_to_eq: forall (A B C: Type) (a1 a2: A) (b1 b2: B) (c1 c2: C),
-  Some (a1, b1, c1) = Some (a2, b2, c2) <-> a1 = a2 /\ b1 = b2 /\ c1 = c2.
-Proof.
-  intros.
-  split.
-  all: intro H.
-  + apply triple_eq_is_eq.
-    apply option_equality_lemma.
-    trivial.
-
-  + apply option_equality_lemma.
-    apply triple_eq_is_eq.
-    trivial.
-Qed.
-
 Lemma correctness_propagation:
   forall (c1 c2: codeBloc), forall (e1 e2 s1 s2: stack),
     code_correctness c1 e1 /\ stack_correctness e1 /\ stack_correctness s1 /\ Some (c1, e1, s1) = Some (c2, e2, s2) -> code_correctness c2 e2 /\ stack_correctness e2 /\ stack_correctness s2.
@@ -134,7 +79,7 @@ Qed.
 
 Lemma code_correctness_propagation_grab:
   forall (c c0: codeBloc), forall (e s: stack),
-    code_correctness (grab c) s -> code_correctness c ((c0, e) # s).
+    code_correctness (Grab; c) s -> code_correctness c ((c0, e) # s).
 Proof.
   move => c c0 e s.
   unfold code_correctness; simpl.
@@ -147,7 +92,7 @@ Qed.
 
 Lemma code_correctness_propagation_push1:
   forall (c1 c2: codeBloc), forall (e: stack),
-    code_correctness (push c1 c2) e -> code_correctness c1 e.
+    code_correctness (Push c1; c2) e -> code_correctness c1 e.
 Proof.
   unfold code_correctness.
   unfold hasAllFreeVarUnder.
@@ -159,7 +104,7 @@ Qed.
 
 Lemma code_correctness_propagation_push2:
   forall (c1 c2: codeBloc), forall (e: stack),
-    code_correctness (push c1 c2) e -> code_correctness c2 e.
+    code_correctness (Push c1; c2) e -> code_correctness c2 e.
 Proof.
   unfold code_correctness.
   unfold hasAllFreeVarUnder.
